db39f96507f7d6350be454bb33dca4f1b18ae7e2
nielsdos <7771979+nielsdos@users.noreply.github.com>
2021-02-19 02:13:03 +0100

    improve

--- TypeLookup.cs
+++ TypeLookup.cs
@@ -14,9 +14,14 @@ namespace CSharpLLVM
         public TypeLookup()
         {
             // TODO
+            typeMap.Add(MetadataType.Boolean, LLVM.Int1Type());
+            typeMap.Add(MetadataType.SByte, LLVM.Int8Type());
+            typeMap.Add(MetadataType.Byte, LLVM.Int8Type());
             typeMap.Add(MetadataType.Int16, LLVM.Int16Type());
             typeMap.Add(MetadataType.Int32, LLVM.Int32Type());
             typeMap.Add(MetadataType.Int64, LLVM.Int64Type());
+            typeMap.Add(MetadataType.Single, LLVM.FloatType());
+            typeMap.Add(MetadataType.Double, LLVM.DoubleType());
             typeMap.Add(MetadataType.Void, LLVM.VoidType());
         }
 
@@ -28,10 +33,12 @@ namespace CSharpLLVM
         public LLVMTypeRef GetLLVMTypeRef(TypeReference typeRef)
         {
             var mdt = typeRef.MetadataType;
-            // TODO: workaround to get it to compile
-            if(!typeMap.ContainsKey(mdt))
+            // TODO: support other things than primitives
+            if(mdt == MetadataType.Class)
+            {
+                // TODO
                 return LLVM.Int64Type();
-            
+            }
             return typeMap[mdt];
         }
     }
