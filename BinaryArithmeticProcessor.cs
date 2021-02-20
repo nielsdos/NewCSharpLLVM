8f08d2113be5d715e98a7a15f1c5f956c1cdd35e
nielsdos <7771979+nielsdos@users.noreply.github.com>
2021-02-20 23:35:09 +0100

    add unsigned div

--- BinaryArithmeticProcessor.cs
+++ BinaryArithmeticProcessor.cs
@@ -3,7 +3,7 @@ using Mono.Cecil.Cil;
 
 namespace CSharpLLVM
 {
-    [InstructionHandler(Code.Add, Code.Sub, Code.Mul, Code.Div)]
+    [InstructionHandler(Code.Add, Code.Sub, Code.Mul, Code.Div, Code.Div_Un)]
     public class BinaryArithmeticProcessor : InstructionProcessor
     {
         public void Process(MethodCompiler compiler, Instruction insn, LLVMBuilderRef builder)
@@ -19,7 +19,9 @@ namespace CSharpLLVM
             else if(insn.OpCode.Code == Code.Mul)
                 result = LLVM.BuildMul(builder, value1.Value, value2.Value, string.Empty);
             else if(insn.OpCode.Code == Code.Div)
-                result = LLVM.BuildSDiv(builder, value1.Value, value2.Value, string.Empty); // TODO: type of div
+                result = LLVM.BuildSDiv(builder, value1.Value, value2.Value, string.Empty);
+            else if(insn.OpCode.Code == Code.Div_Un)
+                result = LLVM.BuildUDiv(builder, value1.Value, value2.Value, string.Empty);
             else
                 result = LLVM.BuildSub(builder, value1.Value, value2.Value, string.Empty);
             compiler.CurrentBasicBlock.GetState().StackPush(new EmulatedStateValue(result, TypeInfo.Primitive));
