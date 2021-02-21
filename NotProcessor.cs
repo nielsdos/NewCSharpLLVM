bf88d54b5fea44844deafb9f39e69b8efcc8d375
nielsdos <7771979+nielsdos@users.noreply.github.com>
2021-02-21 01:08:34 +0100

    not

--- NotProcessor.cs
+++ NotProcessor.cs
@@ -3,17 +3,13 @@ using Mono.Cecil.Cil;
 
 namespace CSharpLLVM
 {
-    [InstructionHandler(Code.Neg)]
-    public class NegProcessor : InstructionProcessor
+    [InstructionHandler(Code.Not)]
+    public class NotProcessor : InstructionProcessor
     {
         public void Process(MethodCompiler compiler, Instruction insn, LLVMBuilderRef builder)
         {
             var value = compiler.CurrentBasicBlock.GetState().StackPop();
-            LLVMValueRef result;
-            if(value.TypeInfo == TypeInfo.FloatingPrimitive)
-                result = LLVM.BuildFNeg(builder, value.Value, string.Empty);
-            else
-                result = LLVM.BuildNeg(builder, value.Value, string.Empty);
+            var result = LLVM.BuildNot(builder, value.Value, string.Empty);
             compiler.CurrentBasicBlock.GetState().StackPush(new EmulatedStateValue(result, value.TypeInfo));
         }
     }
